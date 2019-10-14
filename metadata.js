b14657aad185aa72a80ce6944c76c0e87f62afca
coco-super <sunfeiyu_java@163.com>
2019-10-14 22:44:43 +0800

    fix

--- metadata.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  'name': 'package distribution service',                                 
-  'description': 'Package distribution service for Serverless architecture.',                    
-  'vars': {
-    'service': '{{ projectName }}'
-  }
-}
